5ebf741330c22f4b3a898a8d55a9638c8363dcdd
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-10-17 17:57:54 +0100

    Add planets and properties

--- portfolio_projects/mixed_messages/mixed_messages.js
+++ portfolio_projects/mixed_messages/mixed_messages.js
@@ -1,1 +1,65 @@
+//set-up list of all planets in solar system
+const planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune'];
 
+//set-up properties of each planet
+const properties = {
+    mercury: {
+        color: "light brown",
+        pressure: 10 ** -14,
+        oxygen: 42,
+        sodium: 29,
+        hydrogen: 22,
+        other: 7 
+    },
+    venus: {
+        color: "orange",
+        pressure: 90,
+        carbon_dioxide: 96,
+        nitrogen: 3,
+        other: 1
+    },
+    earth: {
+        color: "blue and green",
+        pressure: 1,
+        nitrogen: 78,
+        oxygen: 21,
+        argon: 1,
+        other: 1 
+    },
+    mars: {
+        color: "red",
+        pressure: 0.006,
+        carbon_dioxide: 95,
+        nitrogen: 3,
+        argon: 1.5,
+        other: 0.5 
+    },
+    jupiter: {
+        color: "light brown",
+        pressure: 1000,
+        hydrogen: 90,
+        helium: 9.5,
+        other: 0.5
+    },
+    saturn: {
+        color: "pale yellow",
+        pressure: 1000,
+        hydrogen: 96,
+        helium: 3,
+        other: 1
+    },
+    uranus: {
+        color: "pale blue",
+        hydrogen: 83,
+        helium: 15,
+        methane: 2.5,
+        other: 0.5
+    },
+    neptune: {
+        color: "blue",
+        hydrogen: 80,
+        helium: 19,
+        methane: 0.5,
+        other: 0.5
+    }
+};
