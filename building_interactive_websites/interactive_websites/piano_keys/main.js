d30ca7caae4dff98daf213c6a09cb9237260b59a
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-10-18 21:27:27 +0100

    Finalise piano keys

--- building_interactive_websites/interactive_websites/piano_keys/main.js
+++ building_interactive_websites/interactive_websites/piano_keys/main.js
@@ -3,27 +3,27 @@ const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'hi
 const notes = [];
 keys.forEach(function(key){
   notes.push(document.getElementById(key));
-})
+});
 
 // Write named functions that change the color of the keys below
 const keyPlay = event => {
-    event.target.style.backgroundColor = 'red';
-  };
-  
-  const keyReturn = event => {
-    event.target.style.backgroundColor = '';
-  };
-  
-  // Write a named function with event handler properties
-  const playNote = (note) => {
-    note.addEventListener('mousedown', keyPlay);
-    note.addEventListener('mouseup', keyReturn);
-  };
-  
-  // Write a loop that runs the array elements through the function
-  notes.forEach(playNote);
+  event.target.style.backgroundColor = 'red';
+};
 
-  // These variables store the buttons that progress the user through the lyrics
+const keyReturn = event => {
+  event.target.style.backgroundColor = '';
+};
+
+// Write a named function with event handler properties
+const playNote = (note) => {
+  note.addEventListener('mousedown', keyPlay);
+  note.addEventListener('mouseup', keyReturn);
+};
+
+// Write a loop that runs the array elements through the function
+notes.forEach(playNote);
+
+// These variables store the buttons that progress the user through the lyrics
 let nextOne = document.getElementById('first-next-line');
 let nextTwo = document.getElementById('second-next-line');
 let nextThree = document.getElementById('third-next-line');
@@ -38,13 +38,44 @@ nextThree.hidden = true;
 startOver.hidden= true;
 
 // Write anonymous event handler property and function for the first progress button
-
+nextOne.addEventListener('click', () => {
+  nextTwo.hidden = false;
+  nextOne.hidden = true;
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('letter-note-six').innerHTML = 'C';
+});
 
 // Write anonymous event handler property and function for the second progress button
-
+nextTwo.addEventListener('click', () => {
+  nextThree.hidden = false;
+  nextTwo.hidden = true;
+  document.getElementById('word-five').innerHTML = 'DEAR';
+  document.getElementById('word-six').innerHTML = 'FRI-';
+  lastLyric.style.display = 'inline-block';
+  document.getElementById('letter-note-three').innerHTML = 'G';
+  document.getElementById('letter-note-four').innerHTML = 'E';
+  document.getElementById('letter-note-five').innerHTML = 'C';
+  document.getElementById('letter-note-six').innerHTML = 'B';
+});
 
 // Write anonymous event handler property and function for the third progress button
-
+nextThree.addEventListener('click', () => {
+  startOver.hidden = false;
+  nextThree.hidden = true;
+  document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('word-three').innerHTML = 'BIRTH';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('word-six').innerHTML = 'YOU!';
+  document.getElementById('letter-note-one').innerHTML = 'F';
+  document.getElementById('letter-note-two').innerHTML = 'F';
+  document.getElementById('letter-note-three').innerHTML = 'E';
+  document.getElementById('letter-note-four').innerHTML = 'C';
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('letter-note-six').innerHTML = 'C';
+  lastLyric.style.display = 'none';
+});
 
 // This is the event handler property and function for the startOver button
 startOver.onclick = function() {
@@ -62,4 +93,4 @@ startOver.onclick = function() {
   document.getElementById('letter-note-five').innerHTML = 'C';
   document.getElementById('word-six').innerHTML = 'YOU!';
   document.getElementById('letter-note-six').innerHTML = 'B';
-}
+};
