29f8b5a653ac031d653cf26bbd946726eca0f021
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-10-25 18:35:23 +0100

    Add subclasses

--- front_end_development/JS_syntax_part_3/build_a_library/library.js
+++ front_end_development/JS_syntax_part_3/build_a_library/library.js
@@ -37,3 +37,57 @@ class Media {
     };
 
 };
+
+class Book extends Media {
+
+    constructor(title, author, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
+    };
+
+    get author() {
+        return this._author;
+    };
+
+    get pages() {
+        return this._pages;
+    };
+
+};
+
+class Movie extends Media {
+
+    constructor(title, director, runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    };
+
+    get director() {
+        return this._director;
+    };
+
+    get runTime() {
+        return this._runTime;
+    };
+
+};
+
+class CD extends Media {
+
+    constructor(title, artist, songs) {
+        super(title);
+        this._artist = artist;
+        this._songs = songs;
+    };
+
+    get artist() {
+        return this._artist;
+    };
+
+    get songs() {
+        return this._songs;
+    };
+
+};
