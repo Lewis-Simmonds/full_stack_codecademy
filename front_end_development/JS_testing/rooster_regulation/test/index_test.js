a8a0bb03f190e0403a3562974375db37980c74b4
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-10-26 20:29:06 +0100

    Finalise rooster regulation with passing tests

--- front_end_development/JS_testing/rooster_regulation/test/index_test.js
+++ front_end_development/JS_testing/rooster_regulation/test/index_test.js
@@ -10,5 +10,31 @@ describe('Rooster', () => {
             const actual = Rooster.announceDawn();
             assert.equal(actual, expected);
         });
+
     });
+
+    describe('.timeAtDawn', () => {
+
+        it('returns its argument as a string', () => {
+            const expected = '8';
+            const actual = Rooster.timeAtDawn(8);
+            assert.strictEqual(actual, expected);
+        });
+
+        it('throws an error if passed a number less than 0', () => {
+            const hour = -5;
+            assert.throws(() => {
+                Rooster.timeAtDawn(hour);
+            }, RangeError);
+        });
+
+        it('throws an error if passed a number greater than 23', () => {
+            const hour = 30;
+            assert.throws(() => {
+                Rooster.timeAtDawn(hour);
+            }, RangeError);
+        });
+
+    });
+
 });
