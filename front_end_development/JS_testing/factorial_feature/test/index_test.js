138315d31f83df81be1b179725b1464368e80563
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-10-26 21:45:29 +0100

    Add first test case

--- front_end_development/JS_testing/factorial_feature/test/index_test.js
+++ front_end_development/JS_testing/factorial_feature/test/index_test.js
@@ -2,7 +2,15 @@ var assert = require("assert");
 var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
+
   describe('.factorial', () => {
 
+    it('will calculate 5! is equal to 120', () => {
+        const expected = 120;
+        const actual = Calculate.factorial(5);
+        assert.strictEqual(actual, expected);
+    });
+
   });
+
 });
