a059484a22d3e1265b38d44b6b96ded0e2a01e52
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-11-01 19:53:38 +0000

    Drop down list functionality checked

--- front_end_development/async_and_http_requests/film_finder/public/script.js
+++ front_end_development/async_and_http_requests/film_finder/public/script.js
@@ -6,25 +6,24 @@ const tmdbBaseUrl = 'https://api.themoviedb.org/3';
 const playBtn = document.getElementById('playBtn');
 
 const getGenres = async () => {
-    const genreRequestEndpoint = '/genre/movie/list';
-    const requestParams = `?api_key=${tmdbKey}`;
-    const urlToFetch = tmdbBaseUrl + genreRequestEndpoint + requestParams;
-    console.log(urlToFetch);
-    try {
-        let response = await fetch(urlToFetch, {method: 'GET'});
-        if (response.ok) {
-            let jsonResponse = await response.json();
-            console.log(jsonResponse);
-            let genres = jsonResponse.genres;
-            console.log(genres);
-        }
-    } catch(error) {
-        console.log(error);
-    };
+  const genreRequestEndpoint = '/genre/movie/list';
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = tmdbBaseUrl + genreRequestEndpoint + requestParams;
+  console.log(urlToFetch);
+  try {
+      let response = await fetch(urlToFetch, {method: 'GET'});
+      if (response.ok) {
+          let jsonResponse = await response.json();
+          let genres = jsonResponse.genres;
+          return genres;
+      }
+  } catch(error) {
+      console.log(error);
+  };
 };
 
 const getMovies = () => {
-  const selectedGenre = getSelectedGenre();
+const selectedGenre = getSelectedGenre();
 
 };
 
@@ -34,10 +33,10 @@ const getMovieInfo = () => {
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
 const showRandomMovie = () => {
-  const movieInfo = document.getElementById('movieInfo');
-  if (movieInfo.childNodes.length > 0) {
-    clearCurrentMovie();
-  };
+const movieInfo = document.getElementById('movieInfo');
+if (movieInfo.childNodes.length > 0) {
+  clearCurrentMovie();
+};
 
 };
 
