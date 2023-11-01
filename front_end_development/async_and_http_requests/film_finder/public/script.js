4eaf3da965b40933b34df152eb14723873c18893
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-11-01 20:01:36 +0000

    Add get movie info functionality

--- front_end_development/async_and_http_requests/film_finder/public/script.js
+++ front_end_development/async_and_http_requests/film_finder/public/script.js
@@ -34,12 +34,24 @@ const getMovies = async () => {
       return movies;
     }
   } catch(error) {
-
+    console.log(error);
   };
 };
 
-const getMovieInfo = () => {
-
+const getMovieInfo = async movie => {
+  const movieId = movie.id;
+  const movieEndpoint = `/movie/${movieId}`;
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = tmdbBaseUrl + movieEndpoint + requestParams;
+  try {
+    let response = await fetch(urlToFetch, {method: 'GET'});
+    if (response.ok) {
+      let movieInfo = await response.json();
+      return movieInfo;
+    }
+  } catch(error) {
+    console.log(error);
+  };
 };
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
