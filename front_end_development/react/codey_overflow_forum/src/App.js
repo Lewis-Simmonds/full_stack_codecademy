3638e5e7f7e4cd0e5de669f547d8dc14605975fe
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-11-05 00:40:02 +0000

    Pass props to header and body

--- front_end_development/react/codey_overflow_forum/src/App.js
+++ front_end_development/react/codey_overflow_forum/src/App.js
@@ -1,2 +1,6 @@
 import React from 'react';
 import {comments} from './CommentData';
+
+comments.map(comment => 
+    <Card commentObject={comment} />
+);
--- front_end_development/react/codey_overflow_forum/src/Card.js
+++ front_end_development/react/codey_overflow_forum/src/Card.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import Header from './Header';
+import Body from './Body';
+
+function Card(props) {
+    return(
+        <div>
+            <Header profileImg={props.commentObject.profileImg} username={props.commentObject.username}/>
+            <Body comment={props.commentObject.comment}/>
+        </div>
+    );
+};
