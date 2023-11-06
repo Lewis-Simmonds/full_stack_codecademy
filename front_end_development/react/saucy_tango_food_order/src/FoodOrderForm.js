5fd6fda541b720e47f6483270c61bb8ac02c8f3d
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-11-06 21:41:31 +0000

    Add basic form

--- front_end_development/react/saucy_tango_food_order/src/FoodOrderForm.js
+++ front_end_development/react/saucy_tango_food_order/src/FoodOrderForm.js
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
 
 function FoodOrderForm() {
+    return (
+        <form>
 
+            <label htmlFor="name">Name:</label>
+            <input id="name" />
+
+            <label htmlFor="phone">Phone Number:</label>
+            <input id="phone" />
+
+            <label htmlFor="address">Address:</label>
+            <input id="address" />
+
+            <label htmlFor="Order">Order:</label>
+            <input id="order" />
+
+            <button type="submit">Submit Order</button>
+            
+        </form>
+    )
 }
 
 export default FoodOrderForm;
