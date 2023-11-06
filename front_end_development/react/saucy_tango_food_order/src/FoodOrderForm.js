90b882ab1b26f5c95d3374ca29d2498c8e081f94
Lewis Simmonds <lslewissimmonds@googlemail.com>
2023-11-06 21:44:32 +0000

    Add states for each input

--- front_end_development/react/saucy_tango_food_order/src/FoodOrderForm.js
+++ front_end_development/react/saucy_tango_food_order/src/FoodOrderForm.js
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 
 function FoodOrderForm() {
+
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [order, setOrder] = useState('');
+
     return (
         <form>
 
             <label htmlFor="name">Name:</label>
-            <input id="name" />
+            <input id="name" value={name} onChange={(e) => setName(e.target.value)}/>
 
             <label htmlFor="phone">Phone Number:</label>
-            <input id="phone" />
+            <input id="phone" value={phone} onChange={(e) => setPhone(e.target.value)}/>
 
             <label htmlFor="address">Address:</label>
-            <input id="address" />
+            <input id="address" value={address} onChange={(e) => setAddress(e.target.value)}/>
 
-            <label htmlFor="Order">Order:</label>
-            <input id="order" />
+            <label htmlFor="order">Order:</label>
+            <input id="order" value={order} onChange={(e) => setOrder(e.target.value)}/>
 
             <button type="submit">Submit Order</button>
-            
+
         </form>
     )
 }
